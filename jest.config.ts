ddec532f3cb5004818d244e7089bb431778d78ad
Jessica Prior <jsscprior@gmail.com>
2022-12-14 17:09:42 -0300

    Aula 2

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  modulePaths: ["src"],
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: "src",
+  testRegex: ".*\\.spec\\.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest",
+  },
+  collectCoverageFrom: ["**/*.(t|j)s"],
+  coverageDirectory: "../coverage",
+  testEnvironment: "node",
+};
